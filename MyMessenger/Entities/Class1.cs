27559f856dac0f59f2ece638206f89513426e22b
Andrey Lazarev <drafter221@gmail.com>
2018-04-20 15:54:02 +0400

    Added entities

--- MyMessenger/Entities/Class1.cs
+++ MyMessenger/Entities/Class1.cs
@@ -9,4 +9,22 @@ namespace Entities
     public class Class1
     {
     }
+
+    public class User
+    {
+        public int Id { get; set; }
+        public string UserName { get; set; }
+        public string Password { get; set; }
+        public string FullName { get; set; }
+    }
+
+    public class Message
+    {
+        public int Id { get; set; }
+        public int From { get; set; }
+        public int To { get; set; }
+        public string Sub { get; set; }
+        public string Body { get; set; }
+        public DateTime Date { get; set; }
+    }
 }
