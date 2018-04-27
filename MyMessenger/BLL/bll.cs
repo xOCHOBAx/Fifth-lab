5f854ff52ae9446ca9c12f045fb862a7c3446fd0
Andrey Lazarev <drafter221@gmail.com>
2018-04-27 22:04:34 +0400

    Vrode final

--- MyMessenger/BLL/bll.cs
+++ MyMessenger/BLL/bll.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 
 namespace BLL
@@ -12,44 +13,69 @@ namespace BLL
 
         public bool AddUser(string UserName, string Password, string FullName)
         {
-            dal = new DAL.DataAccesLayer();
-            return dal.AddUser(UserName, Password, FullName);
+            if (UserName != null && Password != null && FullName != null)
+            {
+                dal = new DAL.DataAccesLayer();
+                return dal.AddUser(UserName, Password, FullName);
+            }
+            return false;
         }
 
         public Entities.User GetUserByName(string UserName)
         {
-            dal = new DAL.DataAccesLayer();
-            return dal.GetUserByName(UserName);
+            if (UserName != null)
+            {
+                dal = new DAL.DataAccesLayer();
+                return dal.GetUserByName(UserName);
+            }
+            return null;
         }
 
         public List<Entities.Message> GetMessagesFromById(int Id)
         {
+            if (Id < 0) return null;
             dal = new DAL.DataAccesLayer();
             return dal.GetMessagesFromById(Id);
         }
 
         public List<Entities.Message> GetMessagesToById(int Id)
         {
+            if (Id < 0) return null;
             dal = new DAL.DataAccesLayer();
             return dal.GetMessagesToById(Id);
         }
 
         public bool Authorize(string UserName, string Password)
         {
-            dal = new DAL.DataAccesLayer();
-            return dal.Authorize(UserName, Password);
+            if (UserName != null && Password != null)
+            {
+                dal = new DAL.DataAccesLayer();
+                return dal.Authorize(UserName, Password);
+            }
+            return false;
         }
 
         public bool AddMessage(int MsgFrom, int MsgTo, string Sub, string Body)
         {
-            dal = new DAL.DataAccesLayer();
-            return dal.AddMessage(MsgFrom,MsgTo,Sub,Body);
+            if (ValidateMessage(Body) && MsgFrom >= 0 && MsgTo >= 0 && ValidateMessage(Sub))
+            {
+                dal = new DAL.DataAccesLayer();
+                return dal.AddMessage(MsgFrom, MsgTo, Sub, Body);
+            }
+            return false;
         }
 
         public Entities.User GetUser(int Id)
         {
             dal = new DAL.DataAccesLayer();
             return dal.GetUser(Id);
+        }
+
+        public bool ValidateMessage(string Body)
+        {
+            var regex = new Regex($"(идиот)", RegexOptions.IgnoreCase);
+            if (regex.IsMatch(Body)) return false;
+            return true;
         }
     }
 }
